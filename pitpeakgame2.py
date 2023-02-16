2ac388d1f2b4aca0310b93bccdf9de122d5da1af
Alex Raisanen <92811641+arraisan@users.noreply.github.com>
2023-02-16 05:33:55 -0700

    Combined Alex and bri's code and sent data to db

---
+++ pitpeakgame2.py
@@ -0,0 +1,165 @@
+import sys
+import tkinter as tk
+from tkinter import PhotoImage
+from tkinter import Text
+from datetime import datetime, date
+import mysql.connector
+import tkinter.messagebox as popupmessagebox
+
+# login test credential
+# sauer.price
+# afcf97aeaa2acfbc9c9f856bcc53744a
+
+# Get today's date
+date = date.today()
+
+conn = mysql.connector.connect(host="107.180.1.16",
+                                   user="springa2023team6",
+                                   password="springa2023team6",
+                                   database="springa2023team6")
+
+# Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+
+def store_pit_text(pittext, peaktext):
+    global pit_text_content, peak_text_content, time
+    now = datetime.now()
+    time = now.strftime("%H:%M:%S")
+    pit_text_content = pittext.get("1.0", 'end-1c')
+    print("Pit text: "+pit_text_content)
+    peak_text_content = peaktext.get("1.0", 'end-1c')
+    print("Peak text: "+peak_text_content)
+
+    pitnpeak_page.destroy()
+
+    # Preparing SQL query to INSERT a record into the database.
+    insert_stmt = (
+        "INSERT INTO feedback(feedID, date, time, user, pittext, peaktext)"
+        "VALUES (%s, %s, %s, %s, %s, %s)"
+    )
+
+    data = (" ", date, time, username, pit_text_content, peak_text_content)
+
+    try:
+        # executing the sql command
+        cursor.execute(insert_stmt, data)
+        # commit changes in database
+        conn.commit()
+    except:
+        conn.rollback()
+
+    thank_you_page = tk.Tk()
+    thank_you_page.geometry("800x770")
+    thank_you_page.title("Thank You Page")
+
+    # Create a label for the title
+    thank_you_title = tk.Label(thank_you_page, text="Thank you for your feedback!", font=("Helvetica", 24))
+    thank_you_title.pack(pady=5)
+
+    # Create a PhotoImage object for the background image
+    image = PhotoImage(file="mountain.png")
+
+    # Create a label to hold the image
+    background_label = tk.Label(thank_you_page, image=image)
+    background_label.pack(fill="both", expand=True)
+
+    thank_you_page.mainloop()
+
+
+def login():
+    global username, tries
+    tries = tries + 1
+    username = userrow_entry.get()
+    password = passrow_entry.get()
+    # Validate user
+    query = "SELECT * FROM employee WHERE username = %s AND password = %s"
+    cursor.execute(query, (username, password))
+    person = cursor.fetchone()
+
+    if person:
+        popupmessagebox.showinfo("Success", "{} Login Successfully!".format(username))
+        go_to_pit_page()
+    elif tries>2:
+        popupmessagebox.showerror("Error", "Too many incorrect login attempts."
+                                           "\nPlease contact Admin. Goodbye...")
+        sys.exit()
+    else:
+        print(tries)
+        popupmessagebox.showerror("Error", "Login credentials incorrect. Try again.")
+
+
+def go_to_pit_page():
+    root.destroy()
+    global pitnpeak_page
+    pitnpeak_page = tk.Tk()
+    pitnpeak_page.geometry("800x770")
+    pitnpeak_page.title("Game Feedback Page")
+
+    # Create a label for the title
+    pit_title = tk.Label(pitnpeak_page, text="Pit: What were the challenging part(s) of the day?", font=("Helvetica", 24))
+    pit_title.pack(pady=5)
+
+    # Create a text box for user input
+    pit_text = Text(pitnpeak_page, height=10, width=40)
+    pit_text.pack(pady=5)
+
+    # Create a label for the title
+    peak_title = tk.Label(pitnpeak_page, text="Peak: What were the best part(s) of the day?", font=("Helvetica", 24))
+    peak_title.pack(pady=5)
+
+    # Create a text box for user input
+    peak_text = Text(pitnpeak_page, height=10, width=40)
+    peak_text.pack(pady=5)
+
+    # Create a submit button
+    submit_button = tk.Button(pitnpeak_page, text="Submit", font=("Helvetica", 16), command=lambda: store_pit_text(pit_text, peak_text))
+    submit_button.pack(pady=5)
+
+    pitnpeak_page.mainloop()
+
+
+# Create the main window
+root = tk.Tk()
+root.geometry("800x770")
+root.title("Welcome page")
+
+# Create a label for the title
+title = tk.Label(root, text="Game: Pit and Peak of the Day", font=("Helvetica", 24))
+title.pack(pady=5)
+
+# Create a label to display today's date
+date_label = tk.Label(root, text="Today's date: " + date.strftime("%B %d, %Y"), font=("Helvetica", 14))
+date_label.pack(pady=5)
+
+# Username Input
+userrow = tk.Label(root, text="Username:", bg="black", fg="white")
+userrow.pack(pady=5)
+userrow_entry = tk.Entry(root, width=30)
+userrow_entry.pack(pady=5)
+
+# Password Input
+passrow = tk.Label(root, text="Password:", bg="black", fg='white')
+passrow.pack(pady=5)
+passrow_entry = tk.Entry(root, show="*", width=30)
+passrow_entry.pack(pady=5)
+
+tries = 0
+
+# Create a button
+enter_button = tk.Button(root, text="Enter", font=("Helvetica", 16), command=login)
+enter_button.pack(pady=5)
+
+# Create a PhotoImage object for the background image
+image = PhotoImage(file="mountain.png")
+
+# Create a label to hold the image
+background_label = tk.Label(root, image=image)
+background_label.pack(fill="both", expand=True)
+
+# Start the main event loop
+root.mainloop()
+
+# login test credential
+# sauer.price
+# afcf97aeaa2acfbc9c9f856bcc53744a
